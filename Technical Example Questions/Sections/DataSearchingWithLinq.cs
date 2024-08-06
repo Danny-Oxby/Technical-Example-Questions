788adab2b6c490cf7820bfcd0d01daf6376ca7dd
DANNY OXBY <b9017792@hallam.shu.ac.uk>
2024-08-06 15:50:53 +0100

    commented unsued code

--- Technical Example Questions/Sections/DataSearchingWithLinq.cs
+++ Technical Example Questions/Sections/DataSearchingWithLinq.cs
@@ -7,6 +7,6 @@ namespace Technical_Example_Questions.Sections
 {
     public class DataSearchingWithLinq
     {
-        private int[] numList = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+        //private int[] numList = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
     }
 }
